aa15076f81d2140734dae936026d3902c8044a4a
Cícero Fahning <cicerofahning@hotmail.com>
2021-03-26 10:24:48 -0300

    Upload de imagem na api

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 from rest_framework import routers
 from books.api import viewsets as bookviewsets
 
@@ -9,4 +12,4 @@ route.register(r'books', bookviewsets.BooksViewSet, basename='Books')
 
 urlpatterns = [
     path('', include(route.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
